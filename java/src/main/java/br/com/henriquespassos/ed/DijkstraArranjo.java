ec1bf6a5525f8dbd66f7ac187ba7351942c866bf
Henrique dos Santos Passos <henrique.passos@fatec.sp.gov.br>
2015-06-07 16:58:36 -0300

    .

--- java/src/main/java/br/com/henriquespassos/ed/DijkstraArranjo.java
+++ java/src/main/java/br/com/henriquespassos/ed/DijkstraArranjo.java
@@ -3,7 +3,8 @@ package br.com.henriquespassos.ed;
 public class DijkstraArranjo {
 
     public static void main(String[] args) {
-        long[][] a = {
+        Grafo g = new Grafo();
+        g.a = new long[][]{
                 {0, 4, 2, Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE},
                 {4, 0, 1, 5, Integer.MAX_VALUE, Integer.MAX_VALUE},
                 {2, 1, 0, 8, 10, Integer.MAX_VALUE},
@@ -11,13 +12,36 @@ public class DijkstraArranjo {
                 {Integer.MAX_VALUE, Integer.MAX_VALUE, 10, 2, 0, 2},
                 {Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE, 6, 2, 0}
         };
+        g.visitado = new boolean[g.a.length];
+        g.no = new No[g.a.length];
 
-        dijkstra(a);
+        int[] total = new int[g.a.length];
+
+        for (int i = 0; i < g.a.length; i++) {
+            g.no[i] = new No();
+            for (int j = 0; j < g.a.length; j++) {
+                if (g.a[i][j] != Integer.MAX_VALUE) {
+                    total[i]++;
+                }
+            }
+        }
+
+        for (int i = 0; i < g.a.length; i++) {
+            g.no[i].vizinho = new No[total[i]];
+            total[i] = 0;
+            for (int j = 0; j < g.a.length; j++) {
+                if (g.a[i][j] != Integer.MAX_VALUE) {
+                    g.no[i].vizinho[total[i]++] = g.no[j];
+                }
+            }
+        }
+
+        //dijkstra(g);
 
         System.out.println(DijkstraArranjo.class.getName());
-        for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < a[i].length; j++) {
-                System.out.printf("%-11s", a[i][j]);
+        for (int i = 0; i < g.a.length; i++) {
+            for (int j = 0; j < g.a[i].length; j++) {
+                System.out.printf("%-11s", g.a[i][j]);
             }
             System.out.println();
         }
--- java/src/main/java/br/com/henriquespassos/ed/Grafo.java
+++ java/src/main/java/br/com/henriquespassos/ed/Grafo.java
@@ -2,6 +2,7 @@ package br.com.henriquespassos.ed;
 
 public class Grafo {
 
+    public long[][] a;
     public boolean[][] adjacente;
     public boolean[] visitado;
     public No[] no;
